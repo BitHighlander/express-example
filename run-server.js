e00fc72a3f561c6b0e2f0dd9b8633ce15e3179d6
Highlander <BitHighlander@users.noreply.github.com>
2022-02-05 13:33:38 -0700

    better payload

--- run-server.js
+++ run-server.js
@@ -8,7 +8,34 @@ app.get('/', (req, res) => {
 });
 
 app.get('/user', (req, res) => {
-    res.send({adddress:"foobar"});
+    res.send(
+        {
+            balances:[
+                {
+                    network:"ETH",
+                    symbol:"ETH",
+                    address:"0x1eB909A73858D8F1e3809684c0fb80b9e5495357",
+                    balance:0.031348861609659481
+                }
+            ],
+            nfts:[
+                {
+                    id:"10411",
+                    name:"pixipet",
+                    url:"https://etherscan.io/token/0x4e76c23fe2a4e37b5e07b5625e17098baab86c18?a=10411"
+                    attributes:[
+                        {
+                            name:"shitty",
+                            value:"+190",
+                        },
+                        {
+                            name:"lasers",
+                            value:"true",
+                        }
+                    ]
+                }
+            ]
+        });
 });
 
 
